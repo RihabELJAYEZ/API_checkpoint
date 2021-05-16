77822d1dffec8517c93dca520b1e516e9a06e8fa
RihabELJAYEZ <rihabeljayez@gmail.com>
2021-05-16 21:47:23 +0200

    first test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const [posts, setPosts]=useState([])
+const getPosts = async () => {
+  try {
+const userPosts = await axios.get("https://jsonplaceholder.typicode.com/users")
+    
+    setPosts(userPosts.data);  // set State
+  
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+  
+  useEffect(()=>{
+    
+    getPosts()
+    const interval=setInterval(()=>{
+      getPosts()
+    },10000)
+    return()=>clearInterval(interval)
+},[])
+
+return (
+  <div>
+  <h1>user List</h1>
+
+  <br></br>
+  <br></br>
+  <div className="first">
+  <Card>
+  <div className="flush">
+  <Card.Body>
+    <h4 style={{color:'red'}}>User Name</h4>
+    {posts.map(post=>(
+      
+      <h6 key={post.id}>{post.username}</h6>
+    
+      ))}
+  </Card.Body>
+  </div>
+  </Card>
+
+
+  <Card>
+  <div className="flush">
+  <Card.Body>
+  <h4 style={{color:'red'}}>User Phone</h4>
+      {posts.map(post=>(
+      <h6 key={post.id}>{post.phone}</h6>
+      ))}
+      
+  </Card.Body>
+  </div>
+</Card>
+
+
+
+  <br></br>
+  <Card>
+  <div>
+  <Card.Body className="flush">
+  <h4 style={{color:'red'}}>User Website</h4>
+      {posts.map(post=>(
+      <h6 key={post.id}>{post.website}</h6>
+      ))}
+      
+  </Card.Body>
+  </div>
+</Card>
+
+<br></br>
+  <Card>
+  <div>
+  <Card.Body className="flush">
+  <h4 style={{color:'red'}}>User Email</h4>
+      {posts.map(post=>(
+      <h6 key={post.id}>{post.email}</h6>
+      ))}
+      
+  </Card.Body>
+  </div>
+</Card>
+
+  <br></br>
+  <br></br>
+  </div>
+  
+
+
+
+
+
+  
+  
+  
+  </div>
+);
 }
 
 export default App;
